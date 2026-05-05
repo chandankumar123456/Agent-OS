e4b36f2fa1241fc7d134ed1c7275f36480f9e705
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2026-05-05 10:49:26 +0530

    chore(middleware): update middleware init

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -1,9 +1,11 @@
 from .auth import APIKeyMiddleware, get_api_keys
 from .rate_limit import RateLimitMiddleware, get_rate_limit
+from .request_logging import RequestLoggingMiddleware
 
 __all__ = [
     "APIKeyMiddleware",
     "get_api_keys",
     "RateLimitMiddleware",
     "get_rate_limit",
+    "RequestLoggingMiddleware",
 ]
