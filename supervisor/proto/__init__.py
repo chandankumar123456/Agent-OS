d4a5908fc9534ece5a8b2ce7f0efefa584846dde
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2026-05-09 18:29:34 +0530

    Add Protocol Buffer definitions for gRPC communication

---
+++ supervisor/proto/__init__.py
@@ -0,0 +1,9 @@
+"""
+Generated protobuf modules for worker communication.
+"""
+
+# Re-export the generated modules
+from . import worker_pb2
+from . import worker_pb2_grpc
+
+__all__ = ['worker_pb2', 'worker_pb2_grpc']
